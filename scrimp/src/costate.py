198ec31829a001a51cc4b890b6d8cc78b163133d
Giuseppe <gferraro2016@gmail.com>
2023-01-31 11:13:38 +0100

    add test_costate and refactor __str__ method in costate class

--- scrimp/src/costate.py
+++ scrimp/src/costate.py
@@ -5,6 +5,7 @@ class CoState(State):
     def __init__(self, name: str, description: str, state: State, substituted=False):
         assert isinstance(state, State), (f'State {state} must be added before its co-state')
         super().__init__(name, description, state.get_kind(), state.get_region(), state.get_mesh_id())
+        del self._costate
         self._state = state
         self._substituted = substituted
 
@@ -12,8 +13,8 @@ class CoState(State):
         where = ''
         if self.get_state().get_region() is not None:
             where = ', in region numbered ' + str(self.get_state().get_region())
-        str1 = f"A co-state variable: {self.get_name()}, describing: {self.get_description()}, associated to state: {self.get_state()}, has been initialized as a: {self.get_kind()}, on mesh: {self.get_mesh_id()}{where}"
-        str2 =f'The constitutive relations between the state: {self.get_state()}, and the co-state: {self.get_name()}, will {not self.get_substituted() * " not"} be substituted for the resolution: variable {self.get_name()}, will {self.get_substituted() * " not"}  be considered as an unknown'
+        str1 = f"A co-state variable: {self.get_name()}, describing: {self.get_description()}, associated to state: {self.get_state().get_name()}, has been initialized as a: {self.get_kind()}, on mesh: {self.get_mesh_id()}{where}"
+        str2 =f'The constitutive relations between the state: {self.get_state().get_name()}, and the co-state: {self.get_name()}, will{(not self.get_substituted()) * " not"} be substituted for the resolution: variable {self.get_name()}, will { (self.get_substituted()) * "not"} be considered as an unknown'
         return str1 + "\n" + str2
 
     def get_state(self):
