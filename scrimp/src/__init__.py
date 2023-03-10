0b80c81e37da34baa7dac19a0d4cfa6c65f86545
Giuseppe <gferraro2016@gmail.com>
2023-03-10 14:20:08 +0100

    format old initi scrimp v1.0, add method for v2.0

--- scrimp/src/__init__.py
+++ scrimp/src/__init__.py
@@ -1,6 +1,22 @@
+import getfem as gf
+import os
+
+
 def set_default_path():
     """
     Set the default path folder for outputs to the path of this file + outputs
     """
 
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'outputs')
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), "outputs")
+
+
+def set_verbose_gf(verbose):
+    """
+    Set the verbosity level of getfem
+
+    :param verbose: the level
+    :type verbose: int
+    """
+
+    gf.util_trace_level(verbose)
+    gf.util_warning_level(verbose)
