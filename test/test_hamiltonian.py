a3e6b03a03f83d321b188e27d7112a8e0bfdbf5b
Giuseppe <gferraro2016@gmail.com>
2023-03-14 11:30:55 +0100

    move test folder outside src folder

---
+++ test/test_hamiltonian.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestTerm(TestCase):
+    def test_get_description(self):
+        self.fail()
+
+    def test_get_expression(self):
+        self.fail()
+
+    def test_get_regions(self):
+        self.fail()
+
+    def test_get_mesh_id(self):
+        self.fail()
+
+    def test_get_values(self):
+        self.fail()
+
+    def test_set_value(self):
+        self.fail()
