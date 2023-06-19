32dac4236b650bbb9c5e267924af7b6670c5b61f
Giuseppe <gferraro2016@gmail.com>
2023-06-19 17:35:09 +0200

    add filedialog to generate_code page

--- scrimp/GUI/generate_code_page.py
+++ scrimp/GUI/generate_code_page.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QComboBox, QLabel, QLineEdit, QGridLayout
+from PyQt5.QtWidgets import QFileDialog, QComboBox, QLabel, QLineEdit, QGridLayout
 from utils.GUI import gui_pages
 
 
@@ -22,9 +22,24 @@ class Window(QtWidgets.QWidget):
 
         layout = QGridLayout()
 
-        label_filename = QLabel('<font size="4"> Name for your dpHs: </font>')
+        label_filename = QLabel(
+            '<font size="4"> Name for your script based on SCRIMP: </font>'
+        )
         self.line_edit_filname = QLineEdit()
-        self.line_edit_filname.setPlaceholderText("Please enter the name")
+        self.line_edit_filname.setPlaceholderText(
+            "Please insert the name for your script"
+        )
+
+        self.button_file_dialog = QtWidgets.QPushButton("Select Folder", self)
+        self.button_file_dialog.clicked.connect(self.get_path)
+
+        label_directory = QLabel('<font size="4"> The selected directory is: </font>')
+        self.line_edit_directory = QLineEdit()
+        self.line_edit_directory.setPlaceholderText(
+            "Insert manually your filepath or click the button on the right to select the directory"
+        )
+
+        self.file_path = ""
 
         self.button_generate = QtWidgets.QPushButton("Generate")
         self.button_generate.clicked.connect(self.generate_code)
@@ -34,6 +49,9 @@ class Window(QtWidgets.QWidget):
 
         layout.addWidget(label_filename, 1, 0)
         layout.addWidget(self.line_edit_filname, 1, 1)
+        layout.addWidget(label_directory, 2, 0)
+        layout.addWidget(self.line_edit_directory, 2, 1)
+        layout.addWidget(self.button_file_dialog, 2, 2)
         layout.addWidget(self.button_generate, 3, 4)
         layout.addWidget(self.button_prev, 3, 3)
 
@@ -46,6 +64,11 @@ class Window(QtWidgets.QWidget):
         layout.addWidget(self.comboBox, 3, 2)
 
         self.setLayout(layout)
+
+    def get_path(self):
+        self.file_path = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        print(self.file_path)
+        self.line_edit_directory.setText(self.file_path)
 
     def text_changed(self, page):  # s is a str
         self.comboBox.setCurrentText("generate_code_page")
