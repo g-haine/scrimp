f070d33ee1a7a4b256cff5a8a4363fb496f71101
Giuseppe <gferraro2016@gmail.com>
2023-06-29 15:40:49 +0200

    add Help to add_parameter page

--- scrimp/GUI/add_parameter_page.py
+++ scrimp/GUI/add_parameter_page.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import (
     QComboBox,
 )
 from PyQt5.QtCore import Qt
-from utils.GUI import gui_pages, gui_width, gui_height
+from utils.GUI import gui_pages, gui_width, gui_height, Help
 
 
 class Window(QtWidgets.QWidget):
@@ -29,7 +29,7 @@ class Window(QtWidgets.QWidget):
         self.setFixedHeight(gui_height)
         # self.setGeometry(100, 100, 600, 300)
 
-        layout = QGridLayout()
+        self.layout = QGridLayout()
 
         # self.line_edit = QLineEdit()
         # layout.addWidget(self.line_edit)
@@ -78,14 +78,14 @@ class Window(QtWidgets.QWidget):
         self.button_prev = QPushButton("< Prev")
         self.button_prev.clicked.connect(self.previous_page)
 
-        layout.addWidget(self.table_parameters, 1, 0, 1, 3)
+        self.layout.addWidget(self.table_parameters, 1, 0, 1, 3)
         # layout.addWidget(cell_double, 1, 3)
-        layout.addWidget(self.button_clear_all, 0, 1)
-        layout.addWidget(self.button_add_parameter, 0, 2, Qt.AlignTop)
-        layout.addWidget(self.button_delete_parameter, 0, 3, Qt.AlignTop)
+        self.layout.addWidget(self.button_clear_all, 0, 1)
+        self.layout.addWidget(self.button_add_parameter, 0, 2, Qt.AlignTop)
+        self.layout.addWidget(self.button_delete_parameter, 0, 3, Qt.AlignTop)
 
-        layout.addWidget(self.button_next, 4, 3)
-        layout.addWidget(self.button_prev, 4, 2)
+        self.layout.addWidget(self.button_next, 4, 3)
+        self.layout.addWidget(self.button_prev, 4, 2)
 
         # create navigation list
         self.comboBox = QComboBox()
@@ -94,9 +94,46 @@ class Window(QtWidgets.QWidget):
 
         # There is an alternate signal to send the text.
         self.comboBox.currentTextChanged.connect(self.text_changed)
-        layout.addWidget(self.comboBox, 4, 1)
+        self.layout.addWidget(self.comboBox, 4, 1)
 
-        self.setLayout(layout)
+        self.setLayout(self.layout)
+
+        self.help = Help(self.layout, 3, 3)
+        self.table_parameters.cellClicked.connect(self.update_help)
+
+    def update_help(self):
+        example = ""
+        col = self.table_parameters.currentColumn()
+
+        if col is not None:
+            text = self.table_parameters.horizontalHeaderItem(col).text()
+            print(f"col:{col},text:{text},selection:Parameter ")
+
+            self.layout.itemAt(self.layout.count() - 1).widget().show()
+            if col == 0:
+                description = "Choose a name for the Parameter "
+
+            elif col == 1:
+                description = "Choose a set of words to describe the Parameter."
+
+            elif col == 2:
+                description = "Choose what is the kind of your state."
+                example = """It could be one of the following list:
+                \n- scalar-field
+                \n- vector-field
+                \n- tensor-field"""
+
+            elif col == 3:
+                description = "Choose an expression for your Parameter ."
+
+            elif col == 4:
+                description = "This is the port at wich the parameter is bounded."
+
+            self.help.updateFields(text, description, example)
+
+        else:
+            self.help.clear()
+            self.layout.itemAt(self.layout.count() - 1).widget().hide()
 
     def text_changed(self, page):  # s is a str
         self.comboBox.setCurrentText("add_parameter_page")
