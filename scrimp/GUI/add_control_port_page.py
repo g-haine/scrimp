c76bce25b923955318818460237c93f57b15ec67
Giuseppe <gferraro2016@gmail.com>
2023-06-29 16:10:04 +0200

    add Help add_control_port page

--- scrimp/GUI/add_control_port_page.py
+++ scrimp/GUI/add_control_port_page.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import (
     QComboBox,
 )
 from PyQt5.QtCore import Qt
-from utils.GUI import gui_pages, gui_width, gui_height
+from utils.GUI import gui_pages, gui_width, gui_height, Help
 
 
 class Window(QtWidgets.QWidget):
@@ -29,7 +29,7 @@ class Window(QtWidgets.QWidget):
         self.setFixedHeight(gui_height)
         # self.setGeometry(100, 100, 600, 300)
 
-        layout = QGridLayout()
+        self.layout = QGridLayout()
 
         # self.line_edit = QLineEdit()
         # layout.addWidget(self.line_edit)
@@ -86,14 +86,14 @@ class Window(QtWidgets.QWidget):
         self.button_prev = QPushButton("< Prev")
         self.button_prev.clicked.connect(self.previous_page)
 
-        layout.addWidget(self.table_control_ports, 1, 0, 1, 3)
+        self.layout.addWidget(self.table_control_ports, 1, 0, 1, 3)
         # layout.addWidget(cell_double, 1, 3)
-        layout.addWidget(self.button_clear_all, 0, 1)
-        layout.addWidget(self.button_add_control_port, 0, 2, Qt.AlignTop)
-        layout.addWidget(self.button_delete_control_port, 0, 3, Qt.AlignTop)
+        self.layout.addWidget(self.button_clear_all, 0, 1)
+        self.layout.addWidget(self.button_add_control_port, 0, 2, Qt.AlignTop)
+        self.layout.addWidget(self.button_delete_control_port, 0, 3, Qt.AlignTop)
 
-        layout.addWidget(self.button_next, 4, 3)
-        layout.addWidget(self.button_prev, 4, 2)
+        self.layout.addWidget(self.button_next, 4, 3)
+        self.layout.addWidget(self.button_prev, 4, 2)
 
         # create navigation list
         self.comboBox = QComboBox()
@@ -102,9 +102,67 @@ class Window(QtWidgets.QWidget):
 
         # There is an alternate signal to send the text.
         self.comboBox.currentTextChanged.connect(self.text_changed)
-        layout.addWidget(self.comboBox, 4, 1)
+        self.layout.addWidget(self.comboBox, 4, 1)
 
-        self.setLayout(layout)
+        self.setLayout(self.layout)
+
+        self.help = Help(self.layout, 3, 3)
+        self.table_control_ports.cellClicked.connect(self.update_help)
+
+    def update_help(self):
+        example = ""
+        col = self.table_control_ports.currentColumn()
+
+        if col is not None:
+            text = self.table_control_ports.horizontalHeaderItem(col).text()
+            print(f"col:{col},text:{text},selection:Control Port")
+
+            self.layout.itemAt(self.layout.count() - 1).widget().show()
+            if col == 0:
+                description = "Choose a name for the Control Port"
+
+            elif col == 1:
+                description = "Choose a letter and its subscript to name the Control."
+                example = "U_B"
+            elif col == 2:
+                description = "Choose a set of words to describe the Control."
+                example = "Temperature"
+
+            elif col == 3:
+                description = (
+                    "Choose a letter and its subscript to name the Observation"
+                )
+                example = "Y_B"
+            elif col == 4:
+                description = "Choose a set of words to describe the Observation."
+                example = "Normal Heat Flux"
+
+            elif col == 5:
+                description = "Choose what is the kind of your Control Port."
+                example = """It could be one of the following list:
+                \n- scalar-field
+                \n- vector-field
+                \n- tensor-field"""
+
+            elif col == 6:
+                description = "the int identifying the region in mesh_id where the control port belong, useful for boundary ports"
+                example = "Default is None."
+
+            elif col == 7:
+                description = (
+                    "Defines wether the Control Port is an 'effort' or a 'flow'."
+                )
+                example = "Default is effort."
+
+            elif col == 8:
+                description = "Choose which is the ID for the mesh that interest your Control Port."
+                example = "Default is 0."
+
+            self.help.updateFields(text, description, example)
+
+        else:
+            self.help.clear()
+            self.layout.itemAt(self.layout.count() - 1).widget().hide()
 
     def text_changed(self, page):  # s is a str
         self.comboBox.setCurrentText("add_control_port_page")
