44ff0b79885aac89b9c3bfaa6880b89f1f0b1dd7
Ghislain HAINE <94449556+g-haine@users.noreply.github.com>
2025-09-30 17:27:44 +0200

    Use relative imports in GUI module

--- scrimp/GUI.py
+++ scrimp/GUI.py
@@ -2,7 +2,7 @@ import re
 import sys
 from PyQt5 import QtWidgets
 import json
-from GUI import (
+from .GUI import (
     welcome_page,
     load_page,
     create_dphs_page,
@@ -21,7 +21,7 @@ from GUI import (
     generate_code_page,
     export_variable_page,
 )
-from utils.GUI import (
+from .utils.GUI import (
     heading,
     black_listed_words,
     text_create_class,
